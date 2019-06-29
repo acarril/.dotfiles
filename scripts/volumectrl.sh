a6b259070e9a5199f5e2c02fa05192f283adbf2a
acarril <acarril@princeton.edu>
2019-06-29 01:28:15 -0400

    set default sink to 0 if none is running

--- scripts/volumectrl.sh
+++ scripts/volumectrl.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Get active sink:
 SINK=$(pactl list short sinks | grep RUNNING | cut -c1-1)
+# If no active sink then set SINK to internal speakers:
+if [ -z "$SINK" ];
+then
+	SINK=0
+fi
 # Modify sink volume:
 pactl set-sink-volume $SINK $1%
 # Store (new) current sink volume:
