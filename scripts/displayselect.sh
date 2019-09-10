74a29bbd82288b0e6bf2522b155dcf50ae54bb65
acarril <acarril@princeton.edu>
2019-09-09 22:30:10 -0400

    add basic script to manually select display/sound config

---
+++ scripts/displayselect.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+choices="Laptop\nTV\nManual"
+
+chosen=$(echo -e "$choices" | dmenu -i -p "Display:")
+
+case "$chosen" in
+    Laptop) xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off && pactl set-card-profile 0 output:analog-stereo ;;
+    TV) xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off && pactl set-card-profile 0 output:hdmi-stereo ;;
+    Manual) arandr ;;
+esac
