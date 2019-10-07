051075d3a4a586b938540580a075372e924d8cae
acarril <acarril@princeton.edu>
2019-10-07 17:32:35 -0400

    relaunch polybar on monitor change

--- scripts/displayselect.sh
+++ scripts/displayselect.sh
@@ -5,8 +5,8 @@ choices="Laptop\nTV\n4k\nManual"
 chosen=$(echo -e "$choices" | dmenu -fn 'Roboto-11' -i -p "Display:")
 
 case "$chosen" in
-    Laptop) xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off && nitrogen --restore && pactl set-card-profile 0 output:analog-stereo ;;
-    TV) xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off && pactl set-card-profile 0 output:hdmi-stereo ;;
-    4k) xrandr --output eDP1 --off --output DP1 --primary --mode 3840x2160 --scale .8x.8 --pos 0x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off && nitrogen --restore ;;
+    Laptop) xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off && nitrogen --restore && pactl set-card-profile 0 output:analog-stereo && $HOME/.config/polybar/launch.sh ;;
+    TV) xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off && pactl set-card-profile 0 output:hdmi-stereo && $HOME/.config/polybar/launch.sh ;;
+    4k) xrandr --output eDP1 --off --output DP1 --primary --mode 3840x2160 --scale .8x.8 --pos 0x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off && nitrogen --restore && $HOME/.config/polybar/launch.sh ;;
     Manual) arandr ;;
 esac
