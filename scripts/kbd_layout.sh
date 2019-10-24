ed1d09ecafb7ce1a43e73edd05cbf7167fa79d3f
acarril <acarril@princeton.edu>
2019-10-24 13:58:19 -0400

    suppress notification from keyboard layout change

--- scripts/kbd_layout.sh
+++ scripts/kbd_layout.sh
@@ -3,4 +3,4 @@
 # Switch keyboard layout:
 (setxkbmap -query | grep -q "variant:\s\+intl") && setxkbmap -layout us || setxkbmap -layout us -variant intl
 # Notify current layout:
-setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}' | while read OUTPUT; do notify-send "Keyboard Layout: $OUTPUT"; done
+# setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}' | while read OUTPUT; do notify-send "Keyboard Layout: $OUTPUT"; done
