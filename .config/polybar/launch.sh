529f35f6668e6147539575100c0c0d677a46cca3
acarril <acarril@princeton.edu>
2019-12-12 21:14:59 -0500

    add script to count monitor and display same polybar on all of them

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,21 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+#    MONITOR=$m polybar --reload mybar &
+#done
+
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload mybar -c ~/.config/polybar/config.ini &
+#  done
+#else
+#  polybar --reload mybar -c ~/.config/polybar/config.ini &
+#fi
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload mybar  -c ~/.config/polybar/config.ini &
+done
+
 # Launch bar(s)
-polybar mybar -c ~/.config/polybar/config.ini &
+#polybar mybar -c ~/.config/polybar/config.ini &
